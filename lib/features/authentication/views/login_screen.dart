912559db32db06a36a9a3e12dded4531d5e37e9e
LontoJ <65587346+LontoJ@users.noreply.github.com>
2023-09-23 17:26:36 +0900

    1. flutter project init 2. go router setting 3. firebase setting 4. riverpod setting

---
+++ lib/features/authentication/views/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeURL = "/login";
+  static const routeName = "login";
+
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Login"),
+      ),
+    );
+  }
+}
